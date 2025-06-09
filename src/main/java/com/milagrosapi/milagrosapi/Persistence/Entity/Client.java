b27f24131a159b41864f3cc958fc699cd02cffad
juanscc2 <jcorteschia@gmail.com>
2025-06-09 09:48:02 -0500

    Creacion de clase Client, para el manejo de JPA

--- src/main/java/com/milagrosapi/milagrosapi/Persistence/Entity/Client.java
+++ src/main/java/com/milagrosapi/milagrosapi/Persistence/Entity/Client.java
@@ -1,4 +1,13 @@
 package com.milagrosapi.milagrosapi.Persistence.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "client")
 public class Client {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer id;
+
+
 }
