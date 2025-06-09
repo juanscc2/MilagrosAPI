982339ee551458e708c1f6fa1ad3e8856ca75ee6
juanscc2 <jcorteschia@gmail.com>
2025-06-09 09:35:13 -0500

    Inicializacion repository

---
+++ src/main/java/com/milagrosapi/milagrosapi/MilagrosapiApplication.java
@@ -0,0 +1,13 @@
+package com.milagrosapi.milagrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilagrosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilagrosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milagrosapi/milagrosapi/MilagrosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milagrosapi.milagrosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilagrosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
